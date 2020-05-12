02e75b5e8bb1b0ec8dbcc68e3dbff12f1bf67523
iansedano <iansedano@gmail.com>
2020-05-12 16:18:43 +0200

    flood fill group builder workking

--- Python/flood_fill_group_builder.py
+++ Python/flood_fill_group_builder.py
@@ -1,3 +1,4 @@
+
 # Assuming grid is always square or rectangle!
 
 grid = [
@@ -7,7 +8,19 @@ grid = [
     [1, 1, 1, 2, 2, 2, 3, 1],
     [1, 8, 8, 2, 2, 2, 5, 3]
 ]
-
+# country value - positions in country
+# 1 - 3
+# 2 - 12
+# 3 - 7
+# 2 - 3
+# 9 - 2
+# 8 - 1
+# 3 - 3
+# 1 - 4
+# 1 - 1
+# 8 - 2
+# 5 - 1
+# 3 - 1
 
 class Country:
 
@@ -18,61 +31,75 @@ class Country:
         self.world_size = world_size
 
     def build_country(self, two_layer_array, checked_list):
-        neighbours_to_check = self.positions
-
+        neighbours_to_check = list(self.positions)
         for n in neighbours_to_check:
-            cardinals = get_cardinals(n, two_layer_array)
-
+            cardinals = self.get_cardinals(n)
             for c in cardinals:
                 if (
-                        c != "edge" and
-                        not in self.positions and
+                        c != 'edge' and
+                        c not in self.positions and
+                        c not in checked_list and
                         two_layer_array[c[0]][c[1]] == self.number):
                     neighbours_to_check.append(c)
 
-                if c != self.starting_pos:
+                if (
+                        c != 'edge' and
+                        c not in self.positions and
+                        c != self.starting_pos and
+                        two_layer_array[c[0]][c[1]] == self.number):
                     self.positions.append(c)
 
             checked_list.append(n)
 
     def get_cardinals(self, pos):
+
+        N, E, S, W = '', '', '', ''
+
         if (pos[0] == 0):
             W = "edge"
-        else if (pos[0] == (self.world_size[0] - 1)):
+        elif (pos[0] == (self.world_size[0] - 1)):
             E = "edge"
 
-        if (pos[1] == (world_size[1] - 1)):
+        if (pos[1] == (self.world_size[1] - 1)):
             S = "edge"
-        else if (pos[1] == 0):
+        elif (pos[1] == 0):
             N = "edge"
 
         if N != "edge":
-            N = (pos[0], pos[1] - 1)
+            N = [pos[0], pos[1] - 1]
         if E != "edge":
-            E = (pos[0] + 1, pos[1])
+            E = [pos[0] + 1, pos[1]]
         if S != "edge":
-            S = (pos[0], pos[1] + 1)
+            S = [pos[0], pos[1] + 1]
         if W != "edge":
-            W = (pos[0] - 1, pos[1])
+            W = [pos[0] - 1, pos[1]]
 
         return [N, E, S, W]
 
 
-def make_coordinate_list(two_layer_array):
+def make_pos_list(two_layer_array):
     pos_list = []
     for row_num, row in enumerate(two_layer_array, start=0):
         for col_num, val in enumerate(row, start=0):
-            pos_list.append([row_num, col_num])
+            pos_list.append((row_num, col_num))
     return(pos_list)
 
 
 def build_countries(two_layer_array):
     world_size = (len(two_layer_array), len(two_layer_array[0]))
     pos_list = make_pos_list(two_layer_array)
-
+    countries = []
     checked_list = []
     for row_num, row in enumerate(two_layer_array, start=0):
         for col_num, val in enumerate(row, start=0):
             if [row_num, col_num] not in checked_list:
                 current_country = Country([row_num, col_num], val, world_size)
                 current_country.build_country(two_layer_array, checked_list)
+                countries.append(current_country)
+
+    return countries
+
+
+countries = build_countries(grid)
+for i, c in enumerate(countries, start=1):
+    print(f"// country {i} // \n           +++  number {c.number}  positions : {c.positions}")
