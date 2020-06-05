0658cc99a948ccc31b44f2b6587cb91edf9bc583
Ian <iansedano@gmail.com>
2020-06-05 13:31:55 +0200

    parent and child components

--- React/parent_child_components.js
+++ React/parent_child_components.js
@@ -1,3 +1,45 @@
+
+// in Footer.js
+
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>
+            <h3>Copyrighto</h3>
+        </footer>
+    )
+}
+
+
+export default Footer
+
+// In App.js
+
+import React from "react"
+
+import Footer from "./Footer"
+
+function App() {
+    return (
+        <div>
+        <h1>Hello</h1>
+        <Footer /> // COMPONENT
+        </div>
+    )
+}
+
+export default App
+
+/*
+
+Convention is that components are in Capitalized camelcase
+whereas html components are lowercase
+
+*/
+
+// in INDEX.js
+
 import React from "react"
 import ReactDOM from "react-dom"
 
@@ -6,16 +48,3 @@ import App from "./App"
 ReactDOM.render(<App />, document.getElementById("root"))
 
 
-
-// In App.js
-
-import React from "react"
-
-
-function App() {
-    return (
-        <h1>Hello</h1>
-    )
-}
-
-export default App
