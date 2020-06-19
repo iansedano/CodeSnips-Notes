08b378952c46db3835cffef68ffeae7f930e552f
Ian <iansedano@gmail.com>
2020-06-19 11:26:07 +0200

    c notes

--- C/for_loop.c
+++ C/for_loop.c
@@ -29,3 +29,20 @@ for (i = 0; i < 10; i++) {
 
 printf("Sum of the array is %d\n", sum); // output => Sum of the array is 55
 
+//
+
+#include <stdio.h>
+
+int main() {
+  int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  int factorial = 1;
+  int i;
+
+  for (i=0;i<10;i++) {
+    factorial *= array[i];
+  }
+
+  printf("10! is %d.\n", factorial);
+}
+
+// OUTPUT "10! is 3628800."
