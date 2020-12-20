e377aea61f7efb3aa9a053948591c1e09d8aab86
ian <iansedano@gmail.com>
2020-12-20 11:38:56 +0100

    java counting cows

---
+++ Java/counting_cows/mastermind.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class mastermind{
+  public static void main(String[] args){
+
+
+    Scanner sc = new Scanner(System.in);
+    int code = sc.nextInt();
+
+
+
+  }
+
+
+  static int[] checker(int attempt, int code) {
+
+    
+    
+  }
+
+}
