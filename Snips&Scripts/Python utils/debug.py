cb3feda446258dbc374818f0a03099043f017bdc
Ian <iansedano@gmail.com>
2021-12-31 11:58:37 +0100

    js and python utils

---
+++ Snips&Scripts/Python utils/debug.py
@@ -0,0 +1,9 @@
+import inspect
+import re
+
+
+def p(x):
+    frame = inspect.currentframe().f_back
+    s = inspect.getframeinfo(frame).code_context[0]
+    r = re.search(r"\((.*)\)", s).group(1)
+    print("{} = {}".format(r, x))
