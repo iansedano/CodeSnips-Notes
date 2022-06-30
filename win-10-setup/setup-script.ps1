60c71b5429e8fb78e622bf246fbad68258147cee
Ian <iansedano@gmail.com>
2022-06-30 14:43:35 +0200

    splitting to admin and non admin script

--- win-10-setup/setup-script.ps1
+++ win-10-setup/setup-script.ps1
@@ -1,24 +1,5 @@
 $StartTime = Get-Date
 
-Invoke-RestMethod get.scoop.sh | Invoke-Expression
-scoop bucket add supabase https://github.com/supabase/scoop-bucket.git
-scoop bucket add nerd-fonts
-
-scoop install less cowsay
-scoop install supabase
-scoop install CascadiaCode-NF
-scoop install gsudo
-scoop install gh
-scoop install pyenv
-scoop install nvm
-scoop install fzf
-
-pyenv update
-pyenv install --quiet 3.10.5 3.9.12
-
-nvm install lts
-
-
 Write-Output "Downloading and installing Chocolatey"
 Invoke-WebRequest -useb community.chocolatey.org/install.ps1 | Invoke-Expression
 choco feature enable -n allowGlobalConfirmation
