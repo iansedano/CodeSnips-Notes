81e531570ecbed40bc265bf20db39defe66d1ffc
Ian <iansedano@gmail.com>
2023-02-12 16:07:14 +0100

    fedora setup

--- fedora-setup/install.sh
+++ fedora-setup/install.sh
@@ -2,16 +2,17 @@ ensure_conf () {
   CONF=$1
   FILE=$2
   TEXT=$3
-  if test -f $FILE
-  then
-    if grep -q $TEXT $FILE
-    then
-      sed -i 's/.*${TEXT}.*/${CONF}'
+  if test -f $FILE; then
+    if grep -q $TEXT $FILE; then
+      sed -i "/$TEXT/s/.*/$CONF/" $FILE
     else
-      echo $CONF >> $FILE 
+      if [ "$(tail -c 1 <$FILE)" != $'\n' ]; then
+        echo -e "" >> $FILE # adds newline
+      fi
+      echo "$CONF" >> $FILE
     fi
   else
-    echo $CONF > $FILE 
+    echo $CONF > $FILE
   fi
 }
 
@@ -65,7 +66,6 @@ packages=(
   spotify
   vlc
   zeal
-  pyenv
   flameshot
   switchdesk
 # gnome-boxes
@@ -75,7 +75,38 @@ for package in "${packages[@]}"; do
   dnf install -yq $package
 done
 
-git clone git@github.com:rupa/z.git ~/tools/z # tools?
+# z
+git clone git@github.com:rupa/z.git /opt/z
+
+# pyenv
+pyenv_packages=(
+  pyenv
+  make
+  gcc
+  zlib-devel
+  bzip2
+  bzip2-devel
+  readline-devel
+  sqlite
+  sqlite-devel
+  openssl-devel
+  tk-devel
+  libffi-devel
+  xz-devel
+  libuuid-devel
+  gdbm-devel
+  libnsl2-devel
+)
+
+for package in "${pyenv_packages[@]}"; do
+  dnf install -yq $package
+done
+
+pyenv update
+pyenv rehash
+LATEST_PYTHON=$(pyenv install --list | grep -E "^\s*[0-9]+\.[0-9]+\.[0-9]+$" | tail -1)
+pyenv install $LATEST_PYTHON
+
 
 # VS Code
 CODE_GPG="https://packages.microsoft.com/keys/microsoft.asc"
@@ -124,4 +155,9 @@ espanso service register
 mkdir temp
 wget https://www.reaper.fm/files/6.x/reaper675_linux_x86_64.tar.xz -P temp
 tar xvf temp/reaper675_linux_x86_64.tar.xz -C temp
-./reaper675_linux_x86_64/install-reaper.sh
+./temp/reaper_linux_x86_64/install-reaper.sh
+
+# Manictime (check version)
+wget https://cdn.manictime.com/setup/linux/v1_4_3_0/ManicTime-arch.tar.gz -P temp
+tar xvf temp/ManicTime-arch.tar.gz -C temp
+./temp/manictime/manictime
