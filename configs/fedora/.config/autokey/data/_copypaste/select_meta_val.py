9af1a54e655373d902044180acddb67a7c2396a7
Ian Currie <iansedano@gmail.com>
2023-12-20 20:29:29 +0100

    autokey scripts

--- configs/fedora/.config/autokey/data/_copypaste/select_meta_val.py
+++ configs/fedora/.config/autokey/data/_copypaste/select_meta_val.py
@@ -2,10 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 
-
-
-
-
 def show_popup(message):
     root = tk.Tk()
     root.withdraw()
@@ -13,11 +9,19 @@ def show_popup(message):
     root.destroy()
 
 
+
+
+
+keyboard.send_keys("<ctrl>+l")
 keyboard.send_keys("<ctrl>+c")
 time.sleep(0.1)
 content = clipboard.get_clipboard()
 
-content = content.split("---", 2)[-1].strip()
+content = content.split(":", 1)[-1].strip()
+if content.startswith(("\"", "'")):
+    content = content[1:]
+if content.endswith(("\"", "'")):
+    content = content[:-1]
 
 clipboard.fill_clipboard(content)
 
