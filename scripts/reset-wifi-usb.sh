85c05568c6a6c7bfc6182f2af896242a3c311695
Ian Currie <iansedano@gmail.com>
2024-09-25 09:12:19 +0200

    update sudo handling for reset wifi

--- scripts/reset-wifi-usb.sh
+++ scripts/reset-wifi-usb.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(realpath "$0")
+
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
+  echo "Switching to root..."
+  sudo "$SCRIPT_PATH" "$@" # Re-run the script with sudo and pass all arguments
+  exit $?                  # Exit with the status code from the sudo command
 fi
+
+echo "Running as root..."
 
 VENDOR_ID="0bda"
 PRODUCT_ID="b812"
