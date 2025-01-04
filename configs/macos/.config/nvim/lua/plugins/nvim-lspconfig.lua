92baedd07971674307146eb9927a8ab051a7239d
Ian Currie <iansedano@gmail.com>
2025-01-04 17:07:39 +0100

    spelling and prep mappings for lspconfig

--- configs/macos/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ configs/macos/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
+  init = function()
+    -- vim.keymap.set("n", "<leader>lf", "<cmd>Format<cr>", { desc = "LSP | Format", silent = true })
+    -- vim.keymap.set("n", "<leader>lF", "<cmd>FormatToggle<cr>", { desc = "LSP | Toggle Autoformat", silent = true })
+    -- vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "LSP | Info", silent = true })
+    -- vim.keymap.set("n", "<leader>lR", "<cmd>LspRestart<cr>", { desc = "LSP | Restart", silent = true })
+  end,
   config = function()
     require("lspconfig").pyright.setup {
       settings = {
