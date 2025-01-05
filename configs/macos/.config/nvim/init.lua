d81a0211127bd940e295cf7c19b170be5906ffda
Ian Currie <iansedano@gmail.com>
2025-01-05 09:49:15 +0100

    add comment to nvim config

--- configs/macos/.config/nvim/init.lua
+++ configs/macos/.config/nvim/init.lua
@@ -1,3 +1,14 @@
+--[[
+nvim config
+
+Author : @iansedano
+
+With help from:
+https://github.com/josean-dev/dev-environment-files
+https://www.ssp.sh/brain/neovim-setup/
+https://github.com/Alexis12119/nvim-config
+--]]
+
 vim.g.mapleader = " "
 
 vim.opt.ignorecase = true
