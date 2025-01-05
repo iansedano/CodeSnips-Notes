62a6809d5703cad4f3a078e05f5c6cc4aae73c32
Ian Currie <iansedano@gmail.com>
2025-01-05 15:09:47 +0100

    clean config and add which-key

--- configs/macos/.config/nvim/lua/plugins/lualine.nvim.lua
+++ configs/macos/.config/nvim/lua/plugins/lualine.nvim.lua
@@ -1,4 +1,45 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      always_show_tabline = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 100,
+        tabline = 100,
+        winbar = 100,
+      }
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { {'filename', path = 1}},
+      lualine_x = { 'filetype' },
+      lualine_y = { },
+      lualine_z = { 'location' }
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+  }
 }
