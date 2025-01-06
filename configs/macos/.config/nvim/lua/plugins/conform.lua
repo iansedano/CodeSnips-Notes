0d1827a0fc4a17ba962933cc4b9d99a5c72d86c5
Ian Currie <iansedano@gmail.com>
2025-01-06 15:11:20 +0100

    add markdown prettier formatting

--- configs/macos/.config/nvim/lua/plugins/conform.lua
+++ configs/macos/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
 			lua = { "stylua" },
 			python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
+			markdown = { "prettierd", "prettier", stop_after_first = true },
 		},
 		format_on_save = {
 			timeout_ms = 500,
