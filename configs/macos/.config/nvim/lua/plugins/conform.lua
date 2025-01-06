32f7228bcc131289b4a44558096e26ba00b27401
Ian Currie <iansedano@gmail.com>
2025-01-06 14:36:56 +0100

    remove ale add conform

---
+++ configs/macos/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
